5475c6fab32512f589f4c9d7f5c1d514fffa45b4
XIXUANHAO <1242358543@qq.com>
2020-08-12 19:36:57 +0800

    msg

---
+++ 74/include/php/main/build-defs.h
@@ -0,0 +1,41 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 7                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) The PHP Group                                          |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Stig Sæther Bakken <ssb@php.net>                             |
+   +----------------------------------------------------------------------+
+*/
+
+#define CONFIGURE_COMMAND " './configure'  '--prefix=/www/server/php/74' '--with-config-file-path=/www/server/php/74/etc' '--enable-fpm' '--with-fpm-user=www' '--with-fpm-group=www' '--enable-mysqlnd' '--with-mysqli=mysqlnd' '--with-pdo-mysql=mysqlnd' '--with-iconv-dir' '--with-freetype' '--with-jpeg' '--with-zlib' '--with-libxml-dir=/usr' '--enable-xml' '--disable-rpath' '--enable-bcmath' '--enable-shmop' '--enable-sysvsem' '--enable-inline-optimization' '--with-curl' '--enable-mbregex' '--enable-mbstring' '--enable-intl' '--enable-ftp' '--enable-gd' '--with-openssl' '--with-mhash' '--enable-pcntl' '--enable-sockets' '--with-xmlrpc' '--enable-soap' '--with-gettext' '--disable-fileinfo' '--enable-opcache' '--with-sodium' '--with-webp' 'PKG_CONFIG_PATH=:/www/server/php/74/lib64/pkgconfig:/www/server/php/74/share/pkgconfig'"
+#define PHP_ODBC_CFLAGS	""
+#define PHP_ODBC_LFLAGS		""
+#define PHP_ODBC_LIBS		""
+#define PHP_ODBC_TYPE		""
+#define PHP_OCI8_DIR			""
+#define PHP_OCI8_ORACLE_VERSION		""
+#define PHP_PROG_SENDMAIL	"/usr/sbin/sendmail"
+#define PEAR_INSTALLDIR         ""
+#define PHP_INCLUDE_PATH	".:"
+#define PHP_EXTENSION_DIR       "/www/server/php/74/lib/php/extensions/no-debug-non-zts-20190902"
+#define PHP_PREFIX              "/www/server/php/74"
+#define PHP_BINDIR              "/www/server/php/74/bin"
+#define PHP_SBINDIR             "/www/server/php/74/sbin"
+#define PHP_MANDIR              "/www/server/php/74/php/man"
+#define PHP_LIBDIR              "/www/server/php/74/lib/php"
+#define PHP_DATADIR             "/www/server/php/74/share/php"
+#define PHP_SYSCONFDIR          "/www/server/php/74/etc"
+#define PHP_LOCALSTATEDIR       "/www/server/php/74/var"
+#define PHP_CONFIG_FILE_PATH    "/www/server/php/74/etc"
+#define PHP_CONFIG_FILE_SCAN_DIR    ""
+#define PHP_SHLIB_SUFFIX        "so"
+#define PHP_SHLIB_EXT_PREFIX    ""
